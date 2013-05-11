943879fc21ef52e5ca46c9be978ee3820b327572
johnwarde <john.warde@gmail.com>
2013-05-11 23:34:36 +0100

    Fixed tabs in header of CSV file - in gen-network.sh

--- gen-network.sh
+++ gen-network.sh
@@ -8,8 +8,6 @@
 #hadoop jar GenerateCoAppearanceNetwork.jar GenerateCoAppearanceNetwork -D match-within-n-lines=10 plays/RomeoJuliet.txt $1
 DESTDIR=~/OnPC/Hadoop
 hadoop fs -get $1/part-00000 $DESTDIR/$1.csv
-echo Source Target  State   Weight > $DESTDIR/gephi_$1.csv
-sed -e "s/undirected\tundirected/undirected/" <$DESTDIR/$1.csv >>$DESTDIR/gephi_$1.csv
-#sed "s/BENVOLIO/JB/" <$DESTDIR/$1.csv >$DESTDIR/gephi_$1.csv
+sed -e "1 iSource\tTarget\tState\tWeight" -e "s/undirected\tundirected/undirected/" <$DESTDIR/$1.csv >>$DESTDIR/gephi_$1.csv
 cat $DESTDIR/gephi_$1.csv
 
