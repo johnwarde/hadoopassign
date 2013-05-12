45ca10125d7aaf9e7407f36400026889deffa059
John Warde <john.warde@gmail.com>
2013-05-12 19:28:00 +0100

    Initial version of all plays processor

---
+++ process-plays.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+##########################
+# Student ID:   D10126532
+# Student Name: John Warde
+# Course Code:  DT230B
+# 
+
+SRCDIR=$1
+JARDIR=~/workspace/D10126532/bin
+HDFSIN=/user/soc/autoplaysin
+HDFSOUT=/user/soc/autoplaysout
+
+FILES=$SRCDIR/*
+for f in $FILES
+do
+  FILENAME=`basename $f .txt`  
+  echo "Processing $FILENAME play ..."
+  hadoop fs -put $SRCDIR/$FILENAME.txt $HDFSIN
+  hadoop jar $JARDIR/GenerateCoAppearanceNetwork.jar literaryanalysis.GenerateCoAppearanceNetwork -D match-within-n-lines=5 $HDFSIN/$FILENAME.txt $HDFSOUT/$FILENAME.csv &
+done
+
